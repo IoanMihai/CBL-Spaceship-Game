82521917c2a9c8fff063c41ed55876684c2bba3d
Mihai Ioan <t.m.ioan@student.tue.nl>
2024-10-10 12:31:12 +0200

    Updated Menu

--- Main.java
+++ Main.java
@@ -1,50 +1,82 @@
 import java.awt.*;
-import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
-class Menu {
+import javax.swing.*;
+import javax.swing.border.EmptyBorder;
+
+class Menu implements ActionListener {
     JFrame frame = new JFrame("SPACESHIP");
     JButton playButton;
     JButton quitButton;
-    JPanel panel;
+    JPanel buttonPanel;
 
     void buildMenu() {
         JLabel title = new JLabel("SPACESHIP", SwingConstants.CENTER);
-        Font titleFont = new Font("Times New Roman", Font.BOLD, 20);
+        Font titleFont = new Font("Serif", Font.BOLD, 50);
         title.setFont(titleFont);
         title.setPreferredSize(new Dimension(1080, 100));
         title.setOpaque(true);
         title.setBackground(Color.RED);
         frame.getContentPane().add(title, BorderLayout.PAGE_START);
+        frame.setPreferredSize(new Dimension(1080, 720));
         
-        panel = new JPanel(new FlowLayout());
-        panel.setLayout(new BoxLayout(panel, BoxLayout.PAGE_AXIS));
-        panel.setPreferredSize(new Dimension(1080, 720));
-        panel.setBackground(Color.BLACK);
-        panel.setOpaque(true);
-        
-        frame.getContentPane().add(panel, BorderLayout.CENTER);
+        buttonPanel = new JPanel();
+        buttonPanel.setLayout(new BoxLayout(buttonPanel, BoxLayout.Y_AXIS));
+        buttonPanel.setBorder(BorderFactory.createEmptyBorder(0, 10, 10, 10));
+        buttonPanel.add(Box.createRigidArea(new Dimension(220, 100)));
 
         playButton = new JButton("PLAY");
         quitButton = new JButton("QUIT");
 
-        playButton.setPreferredSize(new Dimension(1080, 100));
-        quitButton.setPreferredSize(new Dimension(1080, 100));
+        playButton.setPreferredSize(new Dimension(200, 100));
 
-        playButton.setBackground(Color.GREEN);
-        quitButton.setBackground(Color.RED);
+        playButton.setBackground(Color.WHITE);
+        quitButton.setBackground(Color.WHITE);
+        playButton.setFont(new Font("Times New Roman", Font.BOLD, 40));
+        quitButton.setFont(new Font("Times New Roman", Font.BOLD, 40));
 
-        panel.add(Box.createRigidArea(new Dimension(1080, 200)));
-        panel.add(playButton, BorderLayout.SOUTH);
-        panel.add(Box.createRigidArea(new Dimension(1080, 300)));
-        panel.add(quitButton, BorderLayout.SOUTH);
+        buttonPanel.add(playButton);
+        buttonPanel.add(Box.createRigidArea(new Dimension(0, 100)));
+        buttonPanel.add(quitButton);
+        buttonPanel.setBackground(Color.BLACK);
+        buttonPanel.setPreferredSize(new Dimension(frame.getWidth(), frame.getHeight()));
+        
+        Container contentPane = frame.getContentPane();
+        contentPane.add(buttonPanel, BorderLayout.CENTER);
 
         frame.pack();
         frame.setVisible(true);
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        buttonListeners();
+    }
+
+    void buttonListeners() {
+        
+        playButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                //TODO: go to next frame
+            }
+        });
+
+        quitButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                frame.dispose();
+            }
+        });
     }
 
     public static void main(String[] a) {
         new Menu().buildMenu();
     }
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'actionPerformed'");
+    }
+
 
 }
