7592ce136653e399d5400c92c453675f569a6c2f
Mihai Ioan <t.m.ioan@student.tue.nl>
2024-10-17 10:56:46 +0200

    Added class for constants

---
+++ Constants.java
@@ -0,0 +1,8 @@
+public final class Constants {
+    public final static int PLAYER_DEFAULT_PROJECTILE = 1;
+    public final static int ENEMY_DEFAULT_PROJECTILE = 2;
+
+    private Constants() {
+        //Hiding the constructor of the class
+    }
+}
